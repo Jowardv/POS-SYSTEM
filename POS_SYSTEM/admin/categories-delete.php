c264d5693f7e67d05d247e6b04fe1c304fc59775
jiraiyahbt <jiraiyah.tumimbang@one.uze.edu.ph>
2025-11-30 15:49:32 +0800

    delete

---
+++ POS_SYSTEM/admin/categories-delete.php
@@ -0,0 +1,21 @@
+<?php
+
+require '../config/function.php';
+
+$paraResultId = checkParamID('id');
+if (is_numeric($paraResultId)) {
+
+    $categoryId = validate($paraResultId);
+
+    $result = delete(tableName: 'categories', $categoryId);
+    if ($result) {
+        redirect('categories.php', 'category deleted successfully');
+    } else {
+        redirect('categories.php', 'something went wrong');
+    }
+
+} else {
+    redirect('categories.php', 'something went wrong');
+}
+
+?>
