1534f5ea35058ad483b4d9acf3b9dc9ca7a1b1f7
jiraiyahbt <jiraiyah.tumimbang@one.uze.edu.ph>
2025-11-30 12:02:47 +0800

    fixing errors

--- POS_SYSTEM/admin/includes/navbar.php
+++ POS_SYSTEM/admin/includes/navbar.php
@@ -1,54 +1,52 @@
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="index.php">test</a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
 
-    <!-- Sidebar Toggle-->
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle">
-        <i class="fas fa-bars"></i>
-    </button>
+        <!-- Navbar Brand -->
+        <a class="navbar-brand" href="index.php">MyPOS</a>
 
-    <!-- Navbar Search (empty)-->
-    <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0"></form>
+        <!-- Toggler -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <!-- Navbar-->
-    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+        <!-- Navbar Menu -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-        <?php if(isset($_SESSION['LoggedIn'])): ?>
-             <li class="nav-item">
-                <a class="nav-link" href="#"><?= $_SESSION ['loggedInUser']['name']; ?></a>
-             </li>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
-            <li class="nav-item">
-                <a class="nav-link" href="logout.php">Logout</a>
-            </li>
+                <!-- HOME -->
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.php">Home</a>
+                </li>
 
-        <?php else: ?>
+                
+                <?php if(isset($_SESSION['loggedIn'])): ?>
 
-            
-            <li class="nav-item">
-                <a class="nav-link" href="login.php">Login</a>
-            </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <?= $_SESSION['loggedInUser']['name']; ?>
+                        </a>
+                    </li>
 
-        <?php endif; ?>
+                    <!-- LOGOUT -->
+                    <li class="nav-item">
+                        <a class="btn btn-danger" href="logout.php">Logout</a>
+                    </li>
 
-        <!-- Dropdown -->
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
-                data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fas fa-user fa-fw"></i>
-            </a>
-
-            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#!">Settings</a></li>
-                <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                <li><hr class="dropdown-divider" /></li>
-
-                <?php if(isset($_SESSION['auth'])): ?>
-                    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
                 <?php else: ?>
-                    <li><a class="dropdown-item" href="login.php">Login</a></li>
+
+                    <!-- LOGIN -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+
                 <?php endif; ?>
+
             </ul>
-        </li>
-    </ul>
+
+        </div>
+    </div>
 </nav>
