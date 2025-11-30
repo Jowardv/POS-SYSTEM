4a62366790b608555f4039b3433d4d9a5bb56f62
jiraiyahbt <jiraiyah.tumimbang@one.uze.edu.ph>
2025-11-30 11:10:31 +0800

    edited login.php

--- POS_SYSTEM/admin/login.php
+++ POS_SYSTEM/admin/login.php
@@ -1,46 +1,49 @@
 <?php include 'includes/header.php'; ?>
 
-    <div class="py-5">
-        <div class="container mt-5">
-            <div class="row justify-content-center">
-                <div class="col-md-6">
-                  <div class="card shadow rounded -4">
+<div class="py-5">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
 
-                  <?php alertMessage(); ?>
+                <div class="card shadow rounded-4">
+                    <?php alertMessage(); ?>
 
-                    <div class="p-5">  
-                <h4 class="text-dark mb-3"Sign into your POS System</h4>
-                <form action="login-code.php" method="POST">
+                    <div class="p-5">
 
-              <div class="mb-3">
-                <label for="Enter Email Id"></label>
-                 <input type="email" name= "email" class="form-control" required/>
-             </div>
-                      <div class="mb-3">
-                <label for="Enter Password"></label>
-                 <input type="password" name= "password" class="form-control" required/>
-             </div>
-             <div class="my-3">
-                <button type= "submit" name= "loginBtn" class = "btn  btn-primary w-100 mt-2">
-                     Sign In </button>
-            
-             </div>
+                        <h4 class="text-dark mb-3">Sign into your POS System</h4>
 
+                        <form action="login-code.php" method="POST">
 
-                </form>  
-            </div>
-                            
+                            <div class="mb-3">
+                                <label class="form-label">Enter Email ID</label>
+                                <input type="email" name="email" class="form-control" required>
+                            </div>
 
+                            <div class="mb-3">
+                                <label class="form-label">Enter Password</label>
+                                <input type="password" name="password" class="form-control" required>
+                            </div>
 
-                    <h1>Welcome to POS System</h1>
+                            <div class="my-3">
+                                <button type="submit" name="loginBtn" class="btn btn-primary w-100 mt-2">
+                                    Sign In
+                                </button>
+                            </div>
 
-                    <a href="login.php" class="btn btn-primary mt-4">Login</a>
+                        </form>
+
+                    </div>
                 </div>
+
+                <!-- Outside card -->
+                <div class="text-center mt-4">
+                    <h1>Welcome to POS System</h1>
+                    <a href="login.php" class="btn btn-primary mt-3">Login</a>
+                </div>
+
             </div>
-        </div>  
+        </div>
     </div>
-  
+</div>
+
 <?php include 'includes/footer.php'; ?>
-
-
- 
